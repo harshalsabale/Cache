d0c408cd47afebc0879450db2f14c9762c951f62
harshalsabale <30774666+harshalsabale@users.noreply.github.com>
2024-04-03 00:34:15 +0530

    Adding First-In-First-Out Eviction Policy

--- src/main/java/com/cache/eviction/EvictionPolicyFactory.java
+++ src/main/java/com/cache/eviction/EvictionPolicyFactory.java
@@ -1,6 +1,7 @@
 package com.cache.eviction;
 
 import com.cache.enums.EvictionPoliciesEnum;
+import com.cache.eviction.policies.FirstInFirstOutReplacementPolicy;
 import com.cache.eviction.policies.LeastRecentlyUsedReplacementPolicy;
 import com.cache.eviction.policies.RandomReplacementPolicy;
 import com.cache.storage.Storage;
@@ -11,6 +12,7 @@ public class EvictionPolicyFactory<K,V>{
     public EvictionPolicy<K> select(final EvictionPoliciesEnum selectedPolicy, final Storage<K, V> storage) {
         return switch (selectedPolicy) {
             case RR -> new RandomReplacementPolicy<>(storage);
+            case FIFO -> new FirstInFirstOutReplacementPolicy<>(storage);
             default -> new LeastRecentlyUsedReplacementPolicy<>(storage);
         };
     }
